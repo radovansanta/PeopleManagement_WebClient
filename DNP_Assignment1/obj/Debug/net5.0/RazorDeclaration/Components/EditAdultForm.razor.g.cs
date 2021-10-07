c2b6a520d887ec286dc2b29ec6d502ebff637f39
RalGoX1 <293346@viauc.dk>
2021-10-07 23:20:24 +0200

    Updated

--- DNP_Assignment1/obj/Debug/net5.0/RazorDeclaration/Components/EditAdultForm.razor.g.cs
+++ DNP_Assignment1/obj/Debug/net5.0/RazorDeclaration/Components/EditAdultForm.razor.g.cs
@@ -110,7 +110,7 @@ using DNP_Assignment1.Persistence;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 61 "/Users/radovansanta/RiderProjects/DNP_Assignment1/DNP_Assignment1/Components/EditAdultForm.razor"
+#line 91 "/Users/radovansanta/RiderProjects/DNP_Assignment1/DNP_Assignment1/Components/EditAdultForm.razor"
        
 
     [Parameter]
